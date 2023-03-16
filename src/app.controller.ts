ce9a1a4345dd0a5f28d5ac1a9e0517e0ae746a00
caixiangfan <caixiang@ualberta.ca>
2023-03-16 23:36:31 +0000

    add price query end points

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { AsyncResource } from 'async_hooks';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,35 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+    /*
+  Params: 
+    stateid: 
+      - For US, refer to https://www.sinfin.net/railways/world/usa/stateids.html plus the following regions.
+          Region: (ENC) East North Central
+          Region: (ESC) East South Central
+          Region: (MAT) Middle Atlantic
+          Region: (MTN) Mountain
+          Region: (NEW) New England
+          Region: (PACC) Pacific Contiguous
+          Region: (PACN) Pacific Noncontiguous
+          Region: (SAT) South Atlantic
+          Region: (WNC) West North Central
+          Region: (WSC) West South Central
+          Total: (US) United States (not including territory data)
+      - For CA, refer to https://www150.statcan.gc.ca/n1/pub/92-195-x/2011001/geo/prov/tbl/tbl8-eng.htm. Now only AB supported. 
+  */
+  @Get('/query/price/:stateid')
+  async queryRealtimeElectricityPrice(@Param('stateid') stateid: string) {
+    let price: number = 0;
+    switch (stateid) {
+      case "AB":
+        price = await this.appService.queryCAElectricityPrice();
+        break;
+      default:
+        price = await this.appService.queryUSElectricityPrice(stateid);
+    }
+    return price;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Logger } from '@nestjs/common';
+import axios from 'axios';
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async queryCAElectricityPrice() {
+    this.logger.debug('Called when the current second is 45');
+    let url_ravg = 'http://ets.aeso.ca/ets_web/ip/Market/Reports/SMPriceReportServlet?contentType=html';
+    try {
+      var re: RegExp = /\d+(\.\d+)/g; // match all floating numbers
+      const maxprice = 1000; // regulated max price: 1000 $/MWh
+      const response = await axios.get(url_ravg);
+      var data = response.data.match(re);
+      let price = +data[1]; // take the latest 30Ravg (30-day rolling average)
+      // if last hour's 30Ravg is not ready, take previous hour's
+      if (price > maxprice) {
+        price = +data[3];
+      }
+      // return price in CAD $/kWh
+      return price/1000; 
+    } catch (exception) {
+        process.stderr.write(`ERROR received from ${url_ravg}: ${exception}\n`);
+    }
+  }
+
+  async queryUSElectricityPrice(stateid: string) {
+    const DEFAULT_API_KEY = "bf4bedWIXe58W0pv6H0YcucpoR7iigCJhbvmuuuv";
+    const APIKey = process.env.EIA_API_KEY ?? DEFAULT_API_KEY;
+    const BASEURL = "https://api.eia.gov/v2/electricity/retail-sales/data?";
+    let url = BASEURL + `api_key=${APIKey}&data[]=price&frequency=monthly&facets[sectorid][]=RES&facets[stateid][]=${stateid}&sort[0][column]=period&sort[0][direction]=desc`;
+    const response = await axios.get(url);
+    const price = response.data.response.data[0].price;
+    // return price in US $/kWh
+    return price/100; 
   }
 }
