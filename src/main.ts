d84fa0fdb21e4c5136bc85840bf2fe9f739295f9
caixiangfan <caixiang@ualberta.ca>
2023-03-19 09:44:49 -0600

    change port to 8080

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerOptions);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
